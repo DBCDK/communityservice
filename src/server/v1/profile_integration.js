0baa4ed212a738213fd82a2c2232d0e6d7bfe80e
Jens Peter Secher <jpsecher@lundogbendsen.dk>
2017-03-03 16:42:10 +0100

    Fixed bug in deletion.

--- src/server/v1/profile_integration.js
+++ src/server/v1/profile_integration.js
@@ -367,6 +367,46 @@ describe('API v1 profile endpoints', () => {
       .end(done);
     });
 
+    it('should mark as deleted when a log exists', done => {
+      service.put(url)
+      .send({modified_by: id, name: 'Change'})
+      .expect(200)
+      .then(() => {
+        service.put(url)
+        .send({modified_by: id})
+        .expect(200)
+        .expect(res => {
+          expectSuccess(res.body, (links, data) => {
+            expect(links).to.have.property('self');
+            expect(links.self).to.equal(url);
+            expectValidate(data, 'v1/schemas/profile-out.json');
+            expect(data).to.have.property('id');
+            expect(data.id).to.equal(id);
+            expect(data).to.have.property('name');
+            expect(data.name).to.equal('Change');
+            expect(data).to.have.property('attributes');
+            expect(data).to.have.property('created_epoch');
+            expect(data.created_epoch).to.match(/^[0-9]+$/);
+            expect(data).to.have.property('modified_epoch');
+            expect(data.modified_epoch).to.match(/^[0-9]+$/);
+            expect(data).to.have.property('modified_by');
+            expect(data.modified_by).to.equal(id);
+            expect(data).to.have.property('deleted_epoch');
+            expect(data.deleted_epoch).to.match(/^[0-9]+$/);
+            expect(data.deleted_epoch).to.not.be.below(data.created_epoch);
+            expect(data).to.have.property('deleted_by');
+            expect(data.deleted_by).to.equal(id);
+            expect(data).to.have.property('log');
+            expect(data.log).to.not.be.null;
+          });
+        })
+        .end(done);
+      })
+      .catch(error => {
+        done(error);
+      });
+    });
+
     it('should update log with minimal attributes', done => {
       const url2 = '/v1/community/1/profile/2';
       // Get original profile.
