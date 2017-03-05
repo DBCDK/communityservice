6e7229584a8c164234fb6f73064ea188aae8dbcf
Jens Peter Secher <jpsecher@lundogbendsen.dk>
2017-03-05 21:21:04 +0100

    Posts & replies.

--- src/server/seeds/integration-big.js
+++ src/server/seeds/integration-big.js
@@ -16,12 +16,12 @@ Eventually we need
 - 1 community
 - 5000 users
 - 500 groups
-- 1 to 100 members per group.
+- 0 to 100 members per group.
 - 20 campaigns
 - 0 to 1000 participants in each campaign.
 - 0 to 500 reviews with 1-5 rating per user.
 - 0 to 200 initial posts per group.
-- 0 to 100 replies to each post recursively.
+- 0 to 100 replies to each post (recursively).
 - 0 to 100 likes per user
 - 0 to 50 follows per user
 
@@ -43,14 +43,15 @@ total: < 370000 entires
 
 const msDbQueryGracePeriod = 30;
 
-const profiles = 10;
+const groups = 100;
+const profiles = 10 * groups;
 const admins = 1;
-const groups = 10;
 const membersPerGroupMax = 5;
-const campaigns = 5;
-const reviewsPerProfileMax = 3;
-const participantsInCampaingsMax = 5;
+const campaigns = 2 * groups;
+const reviewsPerProfileMax = 10;
+const participantsInCampaingsMax = profiles / 10;
 const submissionRateToCampaings = 0.5;
+const postsPerProfileForEachIterationMax = 5;
 
 exports.seed = () => {
   // Indexes: the first index (0) in the array is not used.
@@ -59,26 +60,24 @@ exports.seed = () => {
   let groupToReviews = [];
   let groupToCampaigns = [];
   let profileToCampaigns = [];
+  let groupToPosts = [];
 
   faker.seed(2);
 
-  const communityTable = generateCommunities();
   const adminTable = generateAdminIds();
-  const profileTable = generateProfiles();
-  const groupTable = generateGroups();
-  const memberTable = generateMembersOfGroups();
-  const campaignTable = generateCampaigns();
-  const reviewTable = generateReviews();
-  // const postTable = generatePosts();
-  // const likesTable = generateLikes();
-  // const followsTable = generateFollows();
 
   const service = request(server);
 
   // Communities
-  return Promise.all(communityTable.map(createCommunity))
+  return Promise.resolve(generateCommunities())
+  .then(communityTable => {
+    return Promise.all(communityTable.map(createCommunity));
+  })
   .then(results => {
     console.log(`Created ${results.length} communities`);
+    return generateProfiles();
+  })
+  .then(profileTable => {
     // Profiles
     return sequenceWithDelay(profileTable, msDbQueryGracePeriod, profile => {
       return createProfile(profile);
@@ -89,6 +88,9 @@ exports.seed = () => {
     });
   })
   .then(() => {
+    return generateGroups();
+  })
+  .then(groupTable => {
     // Groups
     return sequenceWithDelay(groupTable, msDbQueryGracePeriod, group => {
       return createEntity(group);
@@ -99,6 +101,9 @@ exports.seed = () => {
     });
   })
   .then(() => {
+    return generateMembersOfGroups();
+  })
+  .then(memberTable => {
     // Members of groups
     return sequenceWithDelay(memberTable, msDbQueryGracePeriod, member => {
       return createAction(member);
@@ -109,6 +114,9 @@ exports.seed = () => {
     });
   })
   .then(() => {
+    return generateCampaigns();
+  })
+  .then(campaignTable => {
     // Campaigns
     return sequenceWithDelay(campaignTable, msDbQueryGracePeriod, campaign => {
       return createEntity(campaign)
@@ -148,16 +156,114 @@ exports.seed = () => {
     });
   })
   .then(() => {
+    return generatePosts();
+  })
+  .then(postTable => {
+    // Posts to groups
+    return sequenceWithDelay(postTable, msDbQueryGracePeriod, post => {
+      return createEntity(post)
+      .then(id => {
+        safePush(groupToPosts, post.entity_ref, Number(id));
+      });
+    }, index => {
+      if (index % 10 === 0) {
+        console.log(`Processed ${index} posts`);
+      }
+    });
+  })
+  .then(() => {
+    return generateReviews();
+  })
+  .then(reviewTable => {
     // Reviews
     return sequenceWithDelay(reviewTable, msDbQueryGracePeriod, review => {
       return createEntity(review)
       .then(id => {
-        // console.log(`Created review ${id} from profile ${review.owner_id} in group ${review.entity_ref}`);
         safePush(groupToReviews, review.entity_ref, Number(id));
       });
     }, index => {
       if (index % 10 === 0) {
         console.log(`Processed ${index} reviews`);
+      }
+    });
+  })
+  .then(() => {
+    return generateReplies();
+  })
+  .then(replyTable => {
+    // Replies to posts.
+    return sequenceWithDelay(replyTable, msDbQueryGracePeriod, post => {
+      return createEntity(post)
+      .then(id => {
+        safePush(groupToPosts, post.attributes.group, Number(id));
+      });
+    }, index => {
+      if (index % 10 === 0) {
+        console.log(`Processed ${index} replies`);
+      }
+    });
+  })
+  .then(() => {
+    return generateReplies();
+  })
+  .then(replyTable => {
+    // More replies to posts.
+    return sequenceWithDelay(replyTable, msDbQueryGracePeriod, post => {
+      return createEntity(post)
+      .then(id => {
+        safePush(groupToPosts, post.attributes.group, Number(id));
+      });
+    }, index => {
+      if (index % 10 === 0) {
+        console.log(`Processed ${index} replies`);
+      }
+    });
+  })
+  .then(() => {
+    return generateReplies();
+  })
+  .then(replyTable => {
+    // Even more replies.
+    return sequenceWithDelay(replyTable, msDbQueryGracePeriod, post => {
+      return createEntity(post)
+      .then(id => {
+        safePush(groupToPosts, post.attributes.group, Number(id));
+      });
+    }, index => {
+      if (index % 10 === 0) {
+        console.log(`Processed ${index} replies`);
+      }
+    });
+  })
+  .then(() => {
+    return generateReplies();
+  })
+  .then(replyTable => {
+    // More, more, more replies.
+    return sequenceWithDelay(replyTable, msDbQueryGracePeriod, post => {
+      return createEntity(post)
+      .then(id => {
+        safePush(groupToPosts, post.attributes.group, Number(id));
+      });
+    }, index => {
+      if (index % 10 === 0) {
+        console.log(`Processed ${index} replies`);
+      }
+    });
+  })
+  .then(() => {
+    return generateReplies();
+  })
+  .then(replyTable => {
+    // More, more, more, more more more replies.
+    return sequenceWithDelay(replyTable, msDbQueryGracePeriod, post => {
+      return createEntity(post)
+      .then(id => {
+        safePush(groupToPosts, post.attributes.group, Number(id));
+      });
+    }, index => {
+      if (index % 10 === 0) {
+        console.log(`Processed ${index} replies`);
       }
     });
   })
@@ -180,6 +286,9 @@ exports.seed = () => {
     console.log('profileToCampaigns');
     console.log(profileToCampaigns);
 
+    console.log('groupToPosts');
+    console.log(groupToPosts);
+
     console.log('success');
   })
   .catch(error => {
@@ -196,11 +305,9 @@ exports.seed = () => {
       service.post('/v1/community/1/entity').send(value)
       .end((error, result) => {
         if (error) {
-          console.log('OST');
           return reject(error);
         }
         if (result.status !== 201) {
-          console.log('BODY');
           return reject(result.body);
         }
         const location = result.header.location;
@@ -210,9 +317,11 @@ exports.seed = () => {
     });
   }
 
-  function createAction(value) {
+  function createAction(value, debug) {
     return new Promise((resolve, reject) => {
-      // console.log(`Creating ${JSON.stringify(value)}`);
+      if (debug) {
+        console.log(`Creating ${JSON.stringify(value)}`);
+      }
       service.post('/v1/community/1/action').send(value)
       .end((error, result) => {
         if (error) {
@@ -224,6 +333,9 @@ exports.seed = () => {
         const location = result.header.location;
         const id = location.match(/action\/(\d+)/)[1];
         resolve(id);
+        if (debug) {
+          console.log(`Created ${id}`);
+        }
       });
     });
   }
@@ -333,10 +445,15 @@ exports.seed = () => {
       const owner_id = faker.random.number() % profiles + 1;
       const past = Date.parse(faker.date.past()) / 1000;
       const future = Date.parse(faker.date.future()) / 1000;
+      const memberIn = profileToGroups[owner_id];
+      // Some profiles are not members of any group.
+      if (!memberIn || memberIn.length === 0) {
+        continue;
+      }
       result.push({
         owner_id,
         type: 'campaign',
-        entity_ref: selectRandomlyFrom(profileToGroups[owner_id]),
+        entity_ref: selectRandomlyFrom(memberIn),
         title: _.capitalize(faker.lorem.words()),
         contents: faker.lorem.paragraphs(2, '\n'),
         start_epoch: past,
@@ -419,6 +536,60 @@ exports.seed = () => {
     return result;
   }
 
+  function generatePosts() {
+    let result = [];
+    for (let profile = 1; profile <= profiles; ++profile) {
+      const memberIn = profileToGroups[profile];
+      // A profile might not be member in any group.
+      if (!memberIn || memberIn.length === 0) {
+        continue;
+      }
+      const random = logDistributedRandom(postsPerProfileForEachIterationMax);
+      for (let i = 0; i < random; ++i) {
+        const group = selectRandomlyFrom(memberIn);
+        result.push({
+          type: 'post',
+          owner_id: profile,
+          entity_ref: group,
+          title: _.capitalize(faker.lorem.words()),
+          contents: faker.lorem.paragraphs(2, '\n')
+        });
+      }
+    }
+    return result;
+  }
+
+  function generateReplies() {
+    let result = [];
+    for (let profile = 1; profile <= profiles; ++profile) {
+      const memberIn = profileToGroups[profile];
+      // A profile might not be member in any group.
+      if (!memberIn || memberIn.length === 0) {
+        continue;
+      }
+      const random = logDistributedRandom(reviewsPerProfileMax);
+      for (let i = 0; i < random; ++i) {
+        const group = selectRandomlyFrom(memberIn);
+        const postsIn = groupToPosts[group];
+        // A group might have no posts.
+        if (!postsIn || postsIn.length === 0) {
+          continue;
+        }
+        const post = selectRandomlyFrom(postsIn);
+        result.push({
+          type: 'post',
+          owner_id: profile,
+          entity_ref: post,
+          title: _.capitalize(faker.lorem.words()),
+          contents: faker.lorem.paragraph(),
+          // Hack to easily find the group when making threads.
+          attributes: {group}
+        });
+      }
+    }
+    return result;
+  }
+
   function generateLikes() {
     return [];
   }
@@ -461,16 +632,27 @@ exports.seed = () => {
     }
   }
 
+  /**
+   * [sequenceWithDelay description]
+   * @param  {Array(α)}           list             Items to process.
+   * @param  {int}                msDelay          Delay in ms between each processing.
+   * @param  {α -> Promise(void)} processing       Processer of one item.
+   * @param  {int -> ()}          progressReporter Optional function, called with index of item.
+   * @return {Promise(void)}                       Promise of the sequential processing.
+   */
   function sequenceWithDelay(list, msDelay, processing, progressReporter) {
     return list.reduce((seq, item, index) => {
       return seq.then(() => {
-        progressReporter(index);
+        if (progressReporter) {
+          progressReporter(index);
+        }
         return new Promise((resolve, reject) => {
           processing(item)
           .then(() => {
             setTimeout(resolve, msDelay);
           })
           .catch(error => {
+            concole.log({inside: error});
             reject(error);
           });
         });
